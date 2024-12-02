d2dfa9b520ed602e58e149c20aa3f7282a0604c4
Quentin  VERDIER <quentin.verdier@conserto.pro>
2024-12-02 16:11:22 +0100

    ✨ Implement steps 1 and 2 for day 1 calculations and enhance output messages

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,16 +1,55 @@
-import { rightList } from "./input.js";
+import { leftList, rightList } from "./input.js";
 
-const orderedFirstList = rightList.toSorted((a, b) => a - b);
-const orderedSecondList = rightList.toSorted((a, b) => a - b);
+// first step of day 1
 
-let result = 0;
+const step1 = () => {
+  console.log("The first step of day 1 is running...");
 
-for (let i = 0; i < orderedFirstList.length; i++) {
-  if (orderedFirstList[i] > orderedSecondList[i]) {
-    result += orderedFirstList[i] - orderedSecondList[i];
-  } else {
-    result += orderedSecondList[i] - orderedFirstList[i];
+  const orderedLeftList = leftList.toSorted((a, b) => a - b);
+  const orderedRightList = rightList.toSorted((a, b) => a - b);
+
+  let result = 0;
+
+  for (let i = 0; i < orderedLeftList.length; i++) {
+    if (orderedLeftList[i] > orderedRightList[i]) {
+      result += orderedLeftList[i] - orderedRightList[i];
+    } else {
+      result += orderedRightList[i] - orderedLeftList[i];
+    }
   }
-}
 
-console.log(`The total distance between the two list is : ${result}`);
+  console.log(`The total distance between the two list is : ${result}`);
+};
+
+step1();
+
+console.log("----------------------------------------------------");
+
+// second step of day 1
+
+const step2 = () => {
+  console.log("The second step of day 1 is running...");
+
+  const leftListLength = leftList.length;
+  const rightMap = new Map();
+
+  for (let i = 0; i < rightList.length; i++) {
+    if (rightMap.has(rightList[i])) {
+      rightMap.set(rightList[i], rightMap.get(rightList[i]) + 1);
+    } else {
+      rightMap.set(rightList[i], 1);
+    }
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < leftListLength; i++) {
+    if (rightMap.has(leftList[i])) {
+      result += leftList[i] * rightMap.get(leftList[i]);
+    }
+  }
+
+  console.log(`The similarity score is : ${result}`);
+};
+
+step2();
