152b196aa817341aa23991ae2916889f0843d9f2
Quentin VERDIER <quent1.verdier@gmail.com>
2024-12-11 15:31:25 +0100

    🎉 Implement step 2 for processing multiplication instructions and results

--- day-3/day-3.js
+++ day-3/day-3.js
@@ -18,6 +18,34 @@ const step1 = () => {
   console.log("----------------------------------------------------");
 };
 
+const step2 = () => {
+  console.log("----------------------------------------------------");
+  console.log("Step 2 : Running...");
+
+  const instructions = Deno.readTextFileSync("day-3/input.txt");
+
+  const regex = /(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/g;
+  const matches = [...instructions.matchAll(regex)];
+
+  let isEnable = true;
+  let resultsSum = 0;
+
+  matches.forEach((match) => {
+    const [fullMatch, , x, y] = match;
+    if (fullMatch === "do()") {
+      isEnable = true;
+    } else if (fullMatch === "don't()") {
+      isEnable = false;
+    } else if (isEnable && fullMatch.startsWith("mul(")) {
+      resultsSum += parseInt(x) * parseInt(y);
+    }
+  });
+
+  console.log(`The the results of the multiplications is : ${resultsSum}`);
+  console.log("----------------------------------------------------");
+};
+
 console.log("----------------------------------------------------");
 console.log("--- Day 3: Mull It Over ---");
 step1();
+step2();
