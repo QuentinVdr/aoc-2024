57113c289c08b3348473807de3d56e5879159c17
Quentin  VERDIER <quentin.verdier@conserto.pro>
2024-12-16 16:31:01 +0100

    ✅ Finish step 1

--- day-4/day-4.js
+++ day-4/day-4.js
@@ -2,69 +2,53 @@ const step1 = () => {
   console.log("----------------------------------------------------");
   console.log("Step 1 : Running...");
 
-  const textLines = Deno.readTextFileSync("day-4/input.txt").split("\r\n");
+  const textLines = Deno.readTextFileSync("day-4/input.txt").split("\n");
 
-  const regex = /XMAS|SAMX/g;
-  const matches = textLines.map((line) => line.matchAll(regex)).flat();
+  let xmasCount = 0;
 
-  let xmasCount = matches.length;
+  function searchXmasInDirection(line, char, lineDirection, charDirection) {
+    if (
+      line + lineDirection * 3 < 0 ||
+      line + lineDirection * 3 >= textLines.length ||
+      char + charDirection * 3 < 0 ||
+      char + charDirection * 3 >= textLines[line].length
+    ) {
+      return 0;
+    }
+    for (const wordSymbol of "XMAS") {
+      if (textLines[line][char] !== wordSymbol) {
+        return 0;
+      }
 
-  for (let line = 0; line < textLines.length - 3; line++) {
+      line += lineDirection;
+      char += charDirection;
+    }
+
+    return 1;
+  }
+
+  for (let line = 0; line < textLines.length; line++) {
     for (let char = 0; char < textLines[line].length; char++) {
-      if (textLines[line][char] === "X") {
-        if (
-          textLines[line + 1][char + 1] === "M" &&
-          textLines[line + 2][char + 2] === "A" &&
-          textLines[line + 3][char + 3] === "S"
-        ) {
-          xmasCount++;
-        }
-        if (
-          textLines[line + 1][char] === "M" &&
-          textLines[line + 2][char] === "A" &&
-          textLines[line + 3][char] === "S"
-        ) {
-          xmasCount++;
-        }
-        if (
-          textLines[line + 1][char - 1] === "M" &&
-          textLines[line + 2][char - 2] === "A" &&
-          textLines[line + 3][char - 3] === "S"
-        ) {
-          xmasCount++;
-        }
-      } else if (textLines[line][char] === "S") {
-        if (
-          textLines[line + 1][char + 1] === "A" &&
-          textLines[line + 2][char + 2] === "M" &&
-          textLines[line + 3][char + 3] === "X"
-        ) {
-          xmasCount++;
-        }
-        if (
-          textLines[line + 1][char] === "A" &&
-          textLines[line + 2][char] === "M" &&
-          textLines[line + 3][char] === "X"
-        ) {
-          xmasCount++;
-        }
-        if (
-          textLines[line + 1][char - 1] === "A" &&
-          textLines[line + 2][char - 2] === "M" &&
-          textLines[line + 3][char - 3] === "X"
-        ) {
-          xmasCount++;
+      for (let lineDirection = -1; lineDirection <= 1; lineDirection++) {
+        for (let charDirection = -1; charDirection <= 1; charDirection++) {
+          if (lineDirection === 0 && charDirection === 0) {
+            continue;
+          }
+          xmasCount += searchXmasInDirection(
+            char,
+            line,
+            lineDirection,
+            charDirection,
+          );
         }
       }
     }
   }
 
-  console.log(`The the results of the multiplications is : ${xmasCount}`);
+  console.log(`The count of "XMAS" is : ${xmasCount}`);
   console.log("----------------------------------------------------");
 };
 
 console.log("----------------------------------------------------");
 console.log("--- Day 4: Ceres Search ---");
 step1();
-
-// Try : 2088 to low
